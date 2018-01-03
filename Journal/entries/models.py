0c00cf019f72ebd206a6dd12381d79289fdd26d7
smflanag <smflanag@gmail.com>
2018-01-03 20:49:15 +0000

    Created and linked Post model

--- Journal/entries/models.py
+++ Journal/entries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    user = models.CharField(max_length=125)
+    post_title = models.CharField(max_length=50)
+    content = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.post_title
