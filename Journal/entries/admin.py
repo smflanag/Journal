9739444c04eb089dfa17e4def5e20ca68a37cf5b
smflanag <smflanag@gmail.com>
2018-01-04 21:53:06 +0000

    User model created, migrated, added to admin

--- Journal/entries/admin.py
+++ Journal/entries/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from entries.models import Post
+from entries.models import Post,User
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Post)
--- Journal/entries/models.py
+++ Journal/entries/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    screen_name = models.CharField(max_length=125)
+    email = models.EmailField()
+    
+
 class Post(models.Model):
-    user = models.CharField(max_length=125)
+    user_name = User.screen_name
     post_title = models.CharField(max_length=50)
     content = models.CharField(max_length=300)
 
     def __str__(self):
         return self.post_title
+
